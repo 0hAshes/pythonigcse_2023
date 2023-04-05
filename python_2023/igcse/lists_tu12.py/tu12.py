89e0b8eb9258220dde65bfc80076e5a07f776223
Vince <75965324+0hAshes@users.noreply.github.com>
2023-04-05 12:54:13 +0800

    Add files via upload

---
+++ python_2023/igcse/lists_tu12.py/tu12.py
@@ -0,0 +1,6 @@
+MyList = ["Kuala Lumpur","London","Paris","New York","Bangkok"]
+
+NewCity = input("Please add a city ")
+MyList.append(NewCity)
+
+print(MyList)
---
+++ python_2023/igcse/lists_tu12.py/tu12_submit.py
@@ -0,0 +1,6 @@
+#Make a list of video game characters that you can input items into.
+
+list = ["bob", "bobina", "bobalina"]
+listes = [["bob", 99, 7, "aaleyah"],["bobina", -10, -10, "araa"],["bobalina", 7, 8, "monkey"]]
+
+print(listes[0][3])
