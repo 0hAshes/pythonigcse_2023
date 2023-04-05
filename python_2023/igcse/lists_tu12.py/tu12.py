2bac9cc50c197caa45ef2f0130267abe3550f392
Vince <75965324+0hAshes@users.noreply.github.com>
2023-04-06 07:49:09 +0800

    Delete python_2023/igcse/lists_tu12.py directory

--- python_2023/igcse/lists_tu12.py/tu12.py
+++
@@ -1,6 +0,0 @@
-MyList = ["Kuala Lumpur","London","Paris","New York","Bangkok"]
-
-NewCity = input("Please add a city ")
-MyList.append(NewCity)
-
-print(MyList)
--- python_2023/igcse/lists_tu12.py/tu12_submit.py
+++
@@ -1,6 +0,0 @@
-#Make a list of video game characters that you can input items into.
-
-list = ["bob", "bobina", "bobalina"]
-listes = [["bob", 99, 7, "aaleyah"],["bobina", -10, -10, "araa"],["bobalina", 7, 8, "monkey"]]
-
-print(listes[0][3])
