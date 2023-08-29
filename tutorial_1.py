b924d6e4072e926a70d6205e8ddca480fc1da383
Araa Asmah Mohd Nasrizal <75965324+Araa-A@users.noreply.github.com>
2023-08-29 14:12:03 +0800

    Create tutorial_1.py

---
+++ tutorial_1.py
@@ -0,0 +1,14 @@
+<<<<<<< HEAD
+myName = input("Please enter your name first name, friend: ")
+
+lastName = input("Now, enter your last name– or else: ")
+
+fullName = myName + " " + lastName 
+
+print("How's it doin'", fullName, "?")
+=======
+vivaldiBorn = 1678
+myAge = int(input('What year were you born in? '))
+ageDifference = myAge - vivaldiBorn
+print("Vivaldi is ", ageDifference,"years older than you. Wow you are so young. What is it like?" )
+>>>>>>> origin/main
