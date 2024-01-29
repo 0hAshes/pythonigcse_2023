f256c78f874572f741ff5860a9bd50206c15d7ea
Araa Asmah Mohd Nasrizal <75965324+Araa-A@users.noreply.github.com>
2024-01-29 12:46:24 +0800

    Create tutorial_12.py

---
+++ tutorial_12.py
@@ -0,0 +1,5 @@
+print("A list? of video game characters from the hit [redacted]?!! \n")
+MyList = ["'illbur", "dori kinnie held hostage in marriage", "has hostages (and nuclear weapons) in marriage", "anarchist", "racoon with affinity for illegal substances", "single ex politician", "toast hallucinator", "regrets leaving to get milk"]
+NewChar = input("Please add a character ")
+MyList.append(NewChar)
+print(*MyList)
