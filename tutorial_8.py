52a7ef3085e848a32e8369f244523a151d22b2fa
Araa Asmah Mohd Nasrizal <75965324+Araa-A@users.noreply.github.com>
2024-01-29 12:45:21 +0800

    Create tutorial_8.py

---
+++ tutorial_8.py
@@ -0,0 +1,17 @@
+#while True:
+#love_status = input ('Do you love me? y/n ')
+  #if love_status == "n" :
+     #break
+
+#excersie 1
+while True:
+ favS = input('What is your favourite subject? ')
+ if favS in ['cs','CS','computer science','Computer science','Computer Science','comp sci','Comp sci', 'Comp Sci']:
+  break
+   
+#excersise 2 
+
+number = 0 #You have to declare this first
+while number < 15.34 or number > 20.54 :
+    print("Please enter a number between 15 and 20")
+    number = float(input("enter a number: "))
