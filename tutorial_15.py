62149197169b1a36fd48ebfe5d8f5bb8449f5406
Araa Asmah Mohd Nasrizal <75965324+Araa-A@users.noreply.github.com>
2024-01-30 09:21:42 +0800

    Update tutorial_15.py

--- tutorial_15.py
+++ tutorial_15.py
@@ -1,1 +1,16 @@
 
+# exercise 1
+x=3.33333333
+x=round(x,3)
+print(x)
+
+# exercise 2 
+
+x=3.33333333
+print(round(x,1))
+
+# self
+
+mynumbers = [1.5,3.5,76.5,4.5,5,5,6.5,7.5]
+for i in mynumbers:
+    print(round(i))
