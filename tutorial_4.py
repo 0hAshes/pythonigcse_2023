837f681134f0e8125c8c93d600e7438664dc900a
Araa Asmah Mohd Nasrizal <75965324+Araa-A@users.noreply.github.com>
2024-02-02 05:20:59 +0800

    Create tutorial_4.py

---
+++ tutorial_4.py
@@ -0,0 +1,4 @@
+vivaldiBorn = 1678
+myAge = int(input('What year were you born in? '))
+ageDifference = myAge - vivaldiBorn
+print("Vivaldi is ", ageDifference,"years older than you. Wow you are so young. What is it like?" )
